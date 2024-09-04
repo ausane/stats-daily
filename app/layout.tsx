355409f2bc176378de5d8bfa6bf84c5193d41856
Vishal <singhvishal9106@gmail.com>
2024-09-04 21:23:21 +0530

    add twitter image

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,18 +12,26 @@ export const metadata: Metadata = {
   description: `Stats Daily is a web application designed to help users track their daily tasks, set targets, and evaluate their work performance. By visualizing their progress and statistics, users can gain insights into their productivity and make necessary changes to improve.`,
   openGraph: {
     title: "StatsDaily",
-    description: "Daily Tasks Completion Tracker.",
+    description: "Daily Tasks Completion Tracker",
     url: "https://stats-daily.vercel.app/",
     siteName: "StatsDaily",
     images: [
       {
         url: "/layout.png",
-        width: 800,
-        height: 600,
-        alt: "Default OG Image",
+        width: 1600,
+        height: 900,
+        alt: "StatsDaily OpenGraph Image",
       },
     ],
     type: "website",
+  },
+  twitter: {
+    title: "StatsDaily",
+    description: "Daily Tasks Completion Tracker",
+    images: {
+      url: "/layout.png",
+      alt: "StatsDaily Twitter Image",
+    },
   },
 };
 
