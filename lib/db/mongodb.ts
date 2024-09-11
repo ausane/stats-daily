d86836a4f1aa912d3e34ce9bf06b8ff9d80c75fa
Vishal <singhvishal9106@gmail.com>
2024-09-11 10:13:47 +0530

    update db connection ts

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -1,23 +1,33 @@
 import mongoose from "mongoose";
 
-const connectToDatabase = async () => {
+export type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+// Extend the NodeJS global object
+declare global {
+  var mongoose: MongooseCache;
+}
+
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+const connectToDatabase = async (): Promise<typeof mongoose> => {
   const uri = process.env.MONGODB_URI;
 
-  if (!uri) {
+  if (!uri)
     throw new Error(
       "Please define the MONGODB_URI environment variable inside .env.local",
     );
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri).then((mongoose) => mongoose);
   }
 
-  if (mongoose.connections[0].readyState) return;
-
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectToDatabase;
