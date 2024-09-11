e7a5124316d32c536a05b532b8e98622adba3cee
Vishal <singhvishal9106@gmail.com>
2024-09-11 09:31:36 +0530

    pro-test

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const user = await stackServerApp.getUser();
   if (!user) return <HomePage />;
+  return <h2>Home Page</h2>;
 
-  const areas = await fetchTasks();
+  // const areas = await fetchTasks();
 
-  return redirect(`/areas/${areas?.length ? areas[0]._id : "create"}`);
+  // return redirect(`/areas/${areas?.length ? areas[0]._id : "create"}`);
 }
