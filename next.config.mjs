847f6123f4ce736cacd5116fbfb09d708ea9b3c6
Vishal <singhvishal9106@gmail.com>
2024-09-15 09:42:25 +0530

    optimize: task list content ui

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
