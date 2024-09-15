921987222c395660cb7b469c8a7b02017a613101
Vishal <singhvishal9106@gmail.com>
2024-09-15 09:57:50 +0530

    fix:cron

--- lib/cron.ts
+++ lib/cron.ts
@@ -6,8 +6,3 @@ cron.schedule("0 0 * * *", async () => {
   console.log("Running dailyStats job at 12 AM");
   await dailyStats();
 });
-
-// cron.schedule("*/2 * * * *", async () => {
-//   console.log("running a task every two minutes");
-//   await dailyStats();
-// });
